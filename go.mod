466d36e48ed841e316c34ab3cc0d5f7407f26143
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-24 19:51:44 +0100

    commit initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gotimezone
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getAllTimezone(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	data := loadData()
+	w.Write(data)
+}
+
+func main() {
+	http.HandleFunc("/", getAllTimezone)
+	fmt.Println("running server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func loadData() []byte {
+	jsonFile, err := os.Open("./response.json")
+
+	if err != nil {
+		log.Println("Not found File", err.Error())
+	}
+	defer jsonFile.Close()
+
+	data, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		log.Println("Not Read File", err.Error())
+	}
+
+	return data
+
+}
